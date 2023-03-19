d44cebf812635cc64b638dba6e518eb5ce3c8dfa
philippe2023 <philippe.webdeveloper@gmail.com>
2023-03-19 17:19:23 +0100

    front end work

---
+++ function.js
@@ -0,0 +1,9 @@
+// Get the Weight (Kg)
+
+// Get the Height (m)
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+var bmi = bmiCalculator(65, 1.8); 
+bmi should equal 20 when it's rounded to the nearest whole number.
+*/
